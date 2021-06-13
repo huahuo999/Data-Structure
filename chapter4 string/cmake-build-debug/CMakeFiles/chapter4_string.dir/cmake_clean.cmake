b3f77275642d2db6c68e1944f6aa261d9d9f4f07
ls <>
2021-06-13 11:41:09 +0800

    第四章  串

---
+++ chapter4 string/cmake-build-debug/CMakeFiles/chapter4_string.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter4_string.dir/main.cpp.obj"
+  "chapter4_string.exe"
+  "chapter4_string.exe.manifest"
+  "chapter4_string.lib"
+  "chapter4_string.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chapter4_string.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
