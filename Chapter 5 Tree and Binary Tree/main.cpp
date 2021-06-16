eda03bcb263b510f30ca9b55053de7ebd8f83bc0
ls <>
2021-06-16 19:45:02 +0800

    Update main.cpp

--- Chapter 5 Tree and Binary Tree/main.cpp
+++ Chapter 5 Tree and Binary Tree/main.cpp
@@ -14,6 +14,37 @@ typedef struct BiTNode{
     struct BiTNode *lchild, *rchild;
 }BiTNode, *BiTree;
 
+
+
+typedef struct ThreadNode{
+    ElemType data;
+    struct ThreadNode *lchild,*rchild;
+    int ltag,rtag;
+}ThreadNode, *ThreadTree;
+
+extern ThreadNode *pre;
+
+void visit(ThreadNode *q){
+    if(q->lchild==nullptr){
+        q->lchild=pre;
+        q->ltag=1;
+    }
+    if(pre!=nullptr&&pre->rchild==nullptr){
+        pre->rchild=q;
+        pre->rtag=1;
+    }
+    pre = q;
+}
+
+void InThread(ThreadTree T){
+    if(T!=NULL){
+        InThread(T->lchild);
+        visit(T);
+        InThread(T->rchild);
+    }
+}
+
+
 int main() {
     BiTree root = nullptr;
     root = (BiTree)malloc(sizeof(BiTNode));
